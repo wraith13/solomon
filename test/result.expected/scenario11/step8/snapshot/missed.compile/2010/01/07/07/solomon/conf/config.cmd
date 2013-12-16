869be59ff8c82c4912574fdd9d8b397c97fc069e
wraith <wraith@trickpalace.net>
2013-12-16 21:20:49 +0900

    [?????????????????????????????????]????? https://bitbucket.org/wraith13/solomon/issue/3/---------------------------------

--- test/result.expected/scenario11/step8/snapshot/missed.compile/2010/01/07/07/solomon/conf/config.cmd
+++ test/result.expected/scenario11/step8/snapshot/missed.compile/2010/01/07/07/solomon/conf/config.cmd
@@ -102,6 +102,9 @@
 @REM 自動コンパイル＆テスト
 @REM SET SOLOMON_AUTO_BUILD=FALSE
 
+@REM 自動コンパイル＆テストでの待機解除時にCLSを実行する
+@REM SET SOLOMON_AUTO_BUILD_WITH_CLS=TRUE
+
 @REM 自動コンパイル＆テストでの監視対象ファイル一覧取得コマンド
 @REM SET SOLOMON_AUTO_BUILD_WATCH_VIEW=DIR /A:-D /S /T:W
 
